3ed44051e2a76bde24f8faa8ef49a7f116322ab0
Karim yousef <karim56780y@gmial.com>
2025-05-17 10:44:39 +0300

    Added new page XYZ

--- src/component/LayoutNavbar.jsx
+++ src/component/LayoutNavbar.jsx
@@ -1,28 +1,25 @@
 // components/ProtectedLayout.js
 import React from "react";
-import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const LayoutNavbar = () => {
+const ProtectedLayout = () => {
   return (
-    <div className="flex flex-col h-screen">
-    
-      <header className="h-16 shadow-md  sm:h-14 text-white">
+    <div className="flex flex-col min-h-screen">
+      <header className="h-16 sm:h-14 shadow-md text-white sticky top-0 z-50 bg-gray-800">
         <Navbar />
       </header>
 
-      <main className="flex-1 overflow-auto pb-4  bg-gray-100">
+      <main className="flex-1 overflow-y-auto bg-gray-100 py-4">
         <Outlet />
       </main>
 
-     <Footer />
-
-       
-   
-    
+      <footer className="bg-gray-800 text-white py-4">
+        <Footer />
+      </footer>
     </div>
   );
 };
 
-export default LayoutNavbar;
+export default ProtectedLayout;
